74867d9b682d18b27264331afb45075216ac9d9b
pawfs <bahriayavx@gmail.com>
2024-02-26 19:11:38 +0100

    add day two counting sort solution

---
+++ day2/CountingSort1.java
@@ -0,0 +1,58 @@
+package day2;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class CountingSort1Result {
+
+    /*
+     * Complete the 'countingSort' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static List<Integer> countingSort(List<Integer> arr) {
+        // Write your code here
+        List<Integer> frequency = new ArrayList<Integer>(Collections.nCopies(100, 0));
+        for (Integer i : arr) {
+            int currentFrequency = frequency.get(i);
+            frequency.set(i, ++currentFrequency);
+        }
+        return frequency;
+    }
+
+}
+
+public class CountingSort1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = CountingSort1Result.countingSort(arr);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                        + "\n");
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}

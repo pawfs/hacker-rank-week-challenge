1de033776665dead13cf4cf87c35893ffee401c5
pawfs <bahriayavx@gmail.com>
2024-03-02 22:11:06 +0100

    add other pairs solution

---
+++ other/Pairs.java
@@ -0,0 +1,72 @@
+package other;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.toList;
+
+class PairsResult {
+
+    /*
+     * Complete the 'pairs' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER k
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static int pairs(int k, List<Integer> arr) {
+    // Write your code here
+        Collections.sort(arr);
+        
+        int i = 0, j = 1, count = 0;
+        
+        while (j < arr.size()) {
+            if (i == j && j < arr.size()-1) {
+                j++;
+            }
+            
+            int diff = arr.get(j) - arr.get(i);
+            
+            if (diff == k) {
+                count++;
+                j++;
+            } else if (diff > k) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        
+        return count;
+
+    }
+
+}
+
+public class Pairs {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = PairsResult.pairs(k, arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
